332a4d1ef6838055e2709fe7648d15281735f405
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-04-15 11:37:53 +0300

    feature(app): add form_for method

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -6,6 +6,20 @@ require 'minitest/autorun'
 module HexletCode
   class Error < StandardError; end
 
+  def self.form_for(_, attributes = {})
+    attr = {}
+    attr[:url] ||= '#'
+    attr[:method] = 'post'
+    attr.merge!(attributes)
+    attr.transform_keys! { |key| key == :url ? :action : key }
+
+    # attr = attributes.transform_keys do |key|
+    #   key == :url ? :action : key
+    # end.merge({ method: 'post' })
+
+    Tag.build('form', attr) {}
+  end
+
   class Tag
     def self.build(tag, attributes = {})
       attr = []
@@ -20,6 +34,24 @@ module HexletCode
 end
 
 class TestTag < Minitest::Test
+  def setup
+    user = Struct.new(:name, :job, keyword_init: true)
+    @user = user.new name: 'John'
+  end
+
+  def test_form_build
+    expect_with_default_action = '<form action="#" method="post"></form>'
+    expect = '<form action="/user" method="post" class="form"></form>'
+    actual = HexletCode.form_for @user do |f|
+    end
+
+    actual2 = HexletCode.form_for @user, url: '/user', class: 'form' do |f|
+    end
+
+    assert_equal actual, expect_with_default_action
+    assert_equal actual2, expect
+  end
+
   def test_simple_tag_build
     expect = '<input type="submit" value="Save">'
     assert_equal HexletCode::Tag.build('br'), '<br>'
