5613537a0fb01610756455e0da722747ae0b66e0
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-04-15 15:20:56 +0300

    refactor(app): move test to dir

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -32,34 +32,3 @@ module HexletCode
     end
   end
 end
-
-class TestTag < Minitest::Test
-  def setup
-    user = Struct.new(:name, :job, keyword_init: true)
-    @user = user.new name: 'John'
-  end
-
-  def test_form_build
-    expect_with_default_action = '<form action="#" method="post"></form>'
-    expect = '<form action="/user" method="post" class="form"></form>'
-    actual = HexletCode.form_for @user do |f|
-    end
-
-    actual2 = HexletCode.form_for @user, url: '/user', class: 'form' do |f|
-    end
-
-    assert_equal actual, expect_with_default_action
-    assert_equal actual2, expect
-  end
-
-  def test_simple_tag_build
-    expect = '<input type="submit" value="Save">'
-    assert_equal HexletCode::Tag.build('br'), '<br>'
-    assert_equal HexletCode::Tag.build('input', type: 'submit', value: 'Save'), expect
-  end
-
-  def test_pair_tag_build
-    expect = '<div id="1" class="link">Pair tag</div>'
-    assert_equal HexletCode::Tag.build('div', id: '1', class: 'link') { 'Pair tag' }, expect
-  end
-end
